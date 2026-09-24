43c701b4c5cebc4f3853ad1515835c23e3f84190
Alessandra59 <alessandra.azevedo114@gmail.com>
2026-09-24 16:14:07 -0300

    Adiciona idade e emailn do Aluno

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 $nome = "Alessandra";
 $idade = 16;
 $curso = "Desenvolvimento de Sistemas";
+$matricula = "2026001";
+$email = "alessandra.azevedo114@gmail.com";
 
 echo "<h1>Sistemas de Cadastro de Alunos</h1>";
 echo "Nome: " . $nome . "<br>";
 echo "Idade: " . $idade . "<br>";
+echo "Matricula: " . $matricula . "<br>";
+echo "email: " . $email . "<br>";
 echo "Curso: " . $curso;
 
 ?>
